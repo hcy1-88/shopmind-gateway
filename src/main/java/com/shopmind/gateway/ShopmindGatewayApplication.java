0458340fb3321a6f08214c4aa5154e69202f7c23
hcy180 <hcy1805110063@163.com>
2025-12-17 19:32:11 +0800

    gateway基础搭建完毕

---
+++ src/main/java/com/shopmind/gateway/ShopmindGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.shopmind.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmindGatewayApplication {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ShopmindGatewayApplication.class, args);
+    }
+}
